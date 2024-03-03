ae28709c58bea6baa16feca4f224e7d077f9c26c
Aryan <64796509+aryanranderiya@users.noreply.github.com>
2024-03-03 23:37:22 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    setSquares("X");
+  }
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square v
